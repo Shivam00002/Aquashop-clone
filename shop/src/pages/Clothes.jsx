77e94262309bc8905672813778bdcfb24a13dfab
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-07-14 22:02:22 +0530

    Update Clothes.jsx

--- shop/src/pages/Clothes.jsx
+++ shop/src/pages/Clothes.jsx
@@ -4,6 +4,7 @@ import { Flex, Grid, Box, Text, Button, Icon, Image } from "@chakra-ui/react";
 import axios from "axios";
 import { AiFillStar } from 'react-icons/ai'
 import { useEffect, useState } from "react";
+
 import { useNavigate } from "react-router-dom";
 import { setItem } from "../utility/localStorage";
 import "../index.css";
